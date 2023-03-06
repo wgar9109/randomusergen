209d626fdec80588c85a044b1a82861d653e6c93
wgar9109 <tylergary09@gmail.com>
2023-03-06 01:31:45 -0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const randomFolks = document.querySelector(".random-peeps");
+const selectUserNumber = document.querySelector("#users");
+
+const getData = async function (numUsers) {
+    const usersRequest = await fetch(`https://randomuser.me/api?results=${numUsers}`
+    );
+    const data = await usersRequest.json();
+
+    const userResults = data.results; // array of objects
+    displayUsers(userResults);
+};
+
+getData();
+
+const displayUsers = function (userResults) {
+    // Clear the randomFolks element
+    randomFolks.innerHTML = "";
+
+    for (const user of userResults) {
+        const country = user.location.country;
+        const name = user.name.first;
+        const imageUrl = user.picture.medium;
+        const userDiv = document.createElement("div");
+        userDiv.innerHTML = `
+        <h3>${name}</h3>
+        <p>${country}</p>
+        <img src=${imageUrl} alt="User avatar" />
+        `;
+        randomFolks.append(userDiv);
+    }
+};
+
+selectUserNumber.addEventListener("change", function (e) {
+    const numUsers = e.target.value
+    getData(numUsers);
+});
